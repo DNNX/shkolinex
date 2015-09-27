487ba3971fc51c86568cb3c758859aef7eb596e7
Viktar Basharymau <viktar.basharymau@gmail.com>
2015-09-27 13:15:12 +0300

    Shkolinex can be run with mis app.start now

--- lib/shkolinex.ex
+++ lib/shkolinex.ex
@@ -1,2 +1,8 @@
 defmodule Shkolinex do
+  use Application
+
+  def start(_type, _args) do
+
+    {:ok, spawn_link fn -> IO.puts("123123123") end}
+  end
 end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule Shkolinex.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger, :httpoison]]
+    [applications: [:logger, :httpoison],
+     mod: {Shkolinex, []}]
   end
 
   # Dependencies can be Hex packages:
